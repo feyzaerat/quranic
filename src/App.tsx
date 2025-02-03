96a3e7f28c6bcdcfb676f3be9f89b1187d9eaae8
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2025-02-04 02:13:52 +0300

    created bootstrap navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import MainLayout from './Layouts/MainLayout'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 type Props = {}
 
 const App = (props: Props) => {
--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Router from '../Routers/Router'
+import { Navbar } from '../Components'
 
 type Props = {}
 
 const MainLayout = (props: Props) => {
   return (
     <>
+    <Navbar/>
     <Router />
     </>
   )
