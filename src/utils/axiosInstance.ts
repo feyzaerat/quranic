67bc4bedb32605740903fad00d3d92568e011a7f
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2025-02-06 00:32:26 +0300

    modified axios

---
+++ src/utils/axiosInstance.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const quranApi = axios.create({
+  baseURL: "https://quranapi.pages.dev/api/",
+})
+const translateApi = axios.create({
+  baseURL: "https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/",
+});
+
+quranApi.interceptors.request.use((config) => {
+  config.headers["Accept-Language"] = "tr";
+  return config;
+});
+translateApi.interceptors.request.use((config) => {
+  config.headers["Accept-Language"] = "tr";
+  return config;
+});
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.log(
+      "An issue was encountered while making the API request : ",
+      error
+    );
+  }
+);
+export  {quranApi, translateApi};
