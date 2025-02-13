c9f28a353807501b4d0dcc16f1cbacdc00c22957
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2025-02-13 03:54:53 +0300

    modified surah names for widgets

--- src/Components/Widgets/QuranCards.tsx
+++ src/Components/Widgets/QuranCards.tsx
@@ -16,28 +16,28 @@ const QuranCards = (props: Props) => {
     { id: 89, title: "Fecr", icon: <GiFlowerEmblem /> },
     { id: 91, title: "Şems", icon: <IoFlowerOutline /> },
     { id: 92, title: "Leyl", icon: <BsFlower1 /> },
-    { id: 93, title: "Duha", icon: <LuFlower /> },
+    { id: 93, title: "Duhâ", icon: <LuFlower /> },
     { id: 94, title: "İnşirah", icon: <IoMdFlower /> },
     { id: 95, title: "Tin", icon: <GiFlowerEmblem /> },
-    { id: 96, title: "Alak", icon: <IoFlowerOutline /> },
+    { id: 96, title: "Alâk", icon: <IoFlowerOutline /> },
     { id: 97, title: "Kadir", icon: <BsFlower1 /> },
     { id: 98, title: "Beyyine", icon: <LuFlower /> },
     { id: 99, title: "Zilzal", icon: <IoMdFlower /> },
     { id: 100, title: "Adiyat", icon: <IoFlowerOutline /> },
-    { id: 101, title: "Karia", icon: <GiFlowerEmblem /> },
+    { id: 101, title: "Kâria", icon: <GiFlowerEmblem /> },
     { id: 102, title: "Tekasür", icon: <LuFlower /> },
     { id: 103, title: "Asr", icon: <BsFlower1 /> },
-    { id: 104, title: "Hümeze", icon: <IoMdFlower /> },
+    { id: 104, title: "Humeze", icon: <IoMdFlower /> },
     { id: 105, title: "Fil", icon: <GiFlowerEmblem /> },
     { id: 106, title: "Kureyş", icon: <IoFlowerOutline /> },
-    { id: 107, title: "Maun", icon: <BsFlower1 /> },
+    { id: 107, title: "Ma'un", icon: <BsFlower1 /> },
     { id: 108, title: "Kevser", icon: <IoFlowerOutline /> },
-    { id: 109, title: "Kafirun", icon: <IoMdFlower /> },
+    { id: 109, title: "Kâfirûn", icon: <IoMdFlower /> },
     { id: 110, title: "Nasr", icon: <GiFlowerEmblem /> },
     { id: 111, title: "Tebbet", icon: <IoFlowerOutline /> },
-    { id: 112, title: "İhlas", icon: <BsFlower1 /> },
-    { id: 113, title: "Felak", icon: <IoFlowerOutline /> },
-    { id: 114, title: "Nas", icon: <IoMdFlower /> },
+    { id: 112, title: "İhlâs", icon: <BsFlower1 /> },
+    { id: 113, title: "Felâk", icon: <IoFlowerOutline /> },
+    { id: 114, title: "Nâs", icon: <IoMdFlower /> },
   ];
   const [page, setPage] = useState(0);
   const itemsPerPage = 6;
@@ -60,13 +60,13 @@ const QuranCards = (props: Props) => {
     <div className="quran-cards d-flex">
       <div className="short__surah layout">
         <div className="header">
-          <p>Kısa Sureler</p>
+          <p>Kısa Sûreler</p>
         </div>
         <div className="content">
           {displayedSurahs.map((s) => (
             <div className="card">
               <div className="card__title">
-                <p>{s.title} Suresi</p>
+                <p>{s.title} Sûresi</p>
               </div>
               <div className="card__content">
                 <Link to={`/surah/${s.id}`}>{s.icon}</Link>
